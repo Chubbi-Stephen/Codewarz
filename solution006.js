d91b1d3a19bf3031268b480bf707a0cf2a0c1ed1
chubbi stephen <udeokoliestephen@gmail.com>
2023-03-09 19:14:24 +0100

    new solution

---
+++ solution006.js
@@ -0,0 +1,16 @@
+// In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+
+// The result should be a string of numbers, separated by comma and space.
+
+
+
+
+// function arithmeticSequenceElements(a, d, n) {
+//   for (i = a; i <= n;) {
+//     console.log(i);
+//   }
+
+  
+// }
+
+// arithmeticSequenceElements(1, 2, 5)
